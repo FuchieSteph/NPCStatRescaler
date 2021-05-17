a1f3ffcde4f613e815670c488817e0150eacf24e
Бугаев Андрей <bugaev.andrei55@gmail.com>
2021-05-17 13:13:34 +0600

    Initial commit

---
+++ NPCStatRescaler/Settings/Settings.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace NPCStatRescaler.Settings
+{
+    public enum Races
+    {
+        Argonian,
+        Breton,
+        DarkElf,
+        HighElf,
+        Imperial,
+        Khajiit,
+        Nord,
+        Orc,
+        Redguard,
+        WoodElf
+    }
+    public class Config
+    {
+        public CommonStats CommonStats { get; set; } = new();
+        
+        public Dictionary<Races, Stats> RacesStats { get; set; } = new()
+        {
+            {Races.Argonian, new Stats()},
+            {Races.Breton, new Stats()},
+            {Races.DarkElf, new Stats()},
+            {Races.HighElf, new Stats()},
+            {Races.Imperial, new Stats()},
+            {Races.Khajiit, new Stats()},
+            {Races.Nord, new Stats()},
+            {Races.Orc, new Stats()},
+            {Races.Redguard, new Stats()},
+            {Races.WoodElf, new Stats()}
+        };
+        
+        public Dictionary<ChildVampire, StatScales> StatScales { get; set; } = new()
+        {
+            {ChildVampire.Child, new StatScales
+            {
+                HealthScale = 0.75f, 
+                MagickaScale = 0.5f, 
+                StaminaScale = 1f, 
+                CarryWeightScale = 0.5f
+            }},
+            {ChildVampire.Vampire, new StatScales
+            {
+                HealthScale = 1f,
+                MagickaScale = 1.5f,
+                StaminaScale = 1.25f,
+                CarryWeightScale = 1f
+            }}
+        };
+
+        public ClassScales Class { get; set; } = new();
+        public NpcOffsetMults NpcOffsetMults { get; set; } = new();
+        public int NpcHealthBonusPerLevel { get; set; } = 0;
+        public HealthRegen HealthRegen { get; set; } = new();
+        public StaminaRegen StaminaRegen { get; set; } = new();
+        public MagickaRegen MagickaRegen { get; set; } = new();
+        public PlayerRegenPenalties PlayerCombatRegenPenalties { get; set; } = new();
+
+
+    }
+}
---
+++ NPCStatRescaler/Settings/SettingsClasses.cs
@@ -0,0 +1,73 @@
+﻿namespace NPCStatRescaler.Settings
+{
+    public enum ChildVampire
+    {
+        Child,
+        Vampire
+    }
+
+    public class CommonStats
+    {
+        public int HealthShift { get; set; } = 50;
+        public float HealthScale { get; set; } = 0.5f;
+        public int StaminaShift { get; set; } = 0;
+        public float StaminaScale { get; set; } = 1;
+        public int MagickaShift { get; set; } = 0;
+        public float MagickaScale { get; set; } = 1;
+    }
+
+    public record Stats
+    {
+        public int Health { get; set; } = 100;
+        public int Stamina { get; set; } = 100;
+        public int Magicka { get; set; } = 100;
+        public int CarryWeight { get; set; } = 200;
+    }
+    public record StatScales
+    {
+        public float HealthScale { get; set; }
+        public float StaminaScale { get; set; }
+        public float MagickaScale { get; set; }
+        public float CarryWeightScale { get; set; }
+    }
+    public record ClassScales
+    {
+        public float HealthScale { get; set; } = 1f;
+        public float StaminaScale { get; set; } = 1.5f;
+        public float MagickaScale { get; set; } = 1.5f;
+    }
+    public record NpcOffsetMults
+    {
+        public float HealthOffsetMult { get; set; } = 0.2f;
+        public float StaminaOffsetMult { get; set; } = 1f;
+        public float MagickaOffsetMult { get; set; } = 1f;
+    }
+
+    public record HealthRegen
+    {
+        public float Scale { get; set; } = 1f;
+        public int Shift { get; set; } = 0;
+        public float PlayableRaceHealthRegenDebuff { get; set; } = 1f;
+        public bool DebuffVampires { get; set; } = false;
+    }
+
+    public record StaminaRegen
+    {
+        public float Scale { get; set; } = 1f;
+        public int Shift { get; set; } = 0;
+    }
+
+    public record MagickaRegen
+    {
+        public float Scale { get; set; } = 0.3f;
+        public int Shift { get; set; } = 0;
+        //public bool ScaleMagickaRegenBuffs { get; set; } = true;
+    }
+
+    public record PlayerRegenPenalties
+    {
+        public float HealthCombatRegenPenalty { get; set; } = 1;
+        public float StaminaCombatRegenPenalty { get; set; } = 1;
+        public float MagickaCombatRegenPenalty { get; set; } = 1;
+    }
+}
